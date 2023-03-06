08ae676ddcc01f058b07ae9121108adad9b64f3a
Marko Kermiciev <kermicievmarko@gmail.com>
2023-03-06 15:26:38 +0100

    setup store file

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter store = {store}>
+  <Provider>
     <React.StrictMode>
       <App />
     </React.StrictMode>
+    </Provider>
   </BrowserRouter>,
 );
