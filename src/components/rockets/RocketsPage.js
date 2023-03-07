58f508b0575b3266560c9b99228b27b37509eb39
Marko Kermiciev <kermicievmarko@gmail.com>
2023-03-07 10:01:52 +0100

    fetch rockets

--- src/components/rockets/RocketsPage.js
+++ src/components/rockets/RocketsPage.js
@@ -1,7 +1,19 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import RocketsFalcon from './RocketsFalcon';
+import { fetchRockets } from '../../redux/rockets/rocketsSlice';
 
-const RocketsPage = () => (
-  <RocketsFalcon />
-);
+const RocketsPage = () => {
+  const dispatch = useDispatch();
+  const status = useSelector((state) => state.rockets.toFetch);
+  useEffect(() => {
+    if (status) {
+      dispatch(fetchRockets());
+    }
+  }, [status, dispatch]);
+  return (
+    <RocketsFalcon />
+  );
+};
 
 export default RocketsPage;
--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -1,11 +1,34 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-const initialState = { rockets: [], isLoading: 'true' };
+const initialState = { rockets: [], toFetch: 'true' };
 
-const rocktsSlice = createSlice({
+export const fetchRockets = createAsyncThunk('rockets/fetchRockets', async () => {
+  const response = await fetch('https://api.spacexdata.com/v3/rockets');
+  return response.json();
+});
+
+const rocketsSlice = createSlice({
   name: 'rockets',
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchRockets.fulfilled, (state, action) => {
+      const newState = { ...state };
+      const rocketsCard = [];
+      action.payload.forEach((rockObj) => {
+        rocketsCard.push({
+          id: rockObj.rocket_id,
+          name: rockObj.rocket_name,
+          type: rockObj.type,
+          image: rockObj.img,
+          isReserved: false,
+        });
+      });
+      newState.rockets = rocketsCard;
+      newState.toFetch = false;
+      return newState;
+    });
+  },
 });
 
-export default rocktsSlice.reducer;
+export default rocketsSlice.reducer;
