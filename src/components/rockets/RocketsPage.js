b1802d9e84ac6e3b0ec21983c17688b93ae9ef4b
Marko Kermiciev <kermicievmarko@gmail.com>
2023-03-08 13:53:06 +0100

    badge and style setup

---
+++ src/components/rockets/RocketsPage.js
@@ -0,0 +1,7 @@
+// // import { useEffect } from 'react';
+// import RocketsFalcon from './RocketsFalcon';
+// // import { fetchRockets } from '../../redux/rockets/rocketsSlice';
+
+// const RocketsPage = () => (<RocketsFalcon />);
+
+// export default RocketsPage;
