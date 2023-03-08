f6258c534d04e4a4edba217f6c9b6cc4af4c4b0b
ram1117 <ramkumark1@outlook.com>
2023-03-08 23:39:01 +0530

    fix eslint errors in test suite file

--- src/__tests__/missions.test.js
+++ src/__tests__/missions.test.js
@@ -6,18 +6,20 @@ import MissionsPage from '../components/missions/MissionsPage';
 import Mission from '../components/missions/Mission';
 
 describe('testing snapshots of Mission components', () => {
-
   it('testing Mission component', () => {
     const mission = renderer.create(<Provider store={store}><Mission /></Provider>).toJSON();
     expect(mission).toMatchSnapshot();
   });
   it('testing MissionsTable component', () => {
-
-    const missionTable = renderer.create(<Provider store={store}><MissionsTable /></Provider>).toJSON();
+    const missionTable = renderer.create(
+      <Provider store={store}><MissionsTable />
+      </Provider>).toJSON();
     expect(missionTable).toMatchSnapshot();
   });
   it('testing MissionsPage component', () => {
-    const missionPage = renderer.create(<Provider store={store}><MissionsPage /></Provider>).toJSON();
+    const missionPage = renderer.create(
+      <Provider store={store}><MissionsPage />
+      </Provider>).toJSON();
     expect(missionPage).toMatchSnapshot();
   });
 });
