88f3f4bfa3f4f31aff90c8559bb5cc2917602fad
ram1117 <ramkumark1@outlook.com>
2023-03-08 22:25:19 +0530

    try to render missiontable with some data

--- src/__tests__/missionUI.test.js
+++ src/__tests__/missionUI.test.js
@@ -1,9 +1,19 @@
 import { screen, render, fireEvent } from '@testing-library/react';
-import renderWithProvider from '../utils/renderWithProvider';
+import  renderWithProvider  from '../utils/test-util';
 import MissionsTable from '../components/missions/MissionsTable';
+import store from '../redux/store';
+import { Provider } from 'react-redux';
+import { fetchMissions } from '../redux/missions/missionSlice';
 
-describe('testing Mission page UI', () => {
+test('testing Mission page UI', () => {
+  const missions = [{
+    id: 'testmission1',
+    name: 'test1',
+    descriptions: 'test mission',
+    isReserved: false,
+    wiki: '',
+  }];
   renderWithProvider(<MissionsTable />);
-  const missionButtons = screen.findAllByText('Status');
-  expect(missionButtons).toHaveLength(1);
+  const buttons = screen.getAllByText('Mission');
+  expect(buttons).toHaveLength(1);
 });
